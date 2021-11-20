a9361de847e4d23a850be4f3e237dbfb6268bf24
M. Nart <tranquocduy184@gmail.com>
2021-11-20 11:49:27 +0700

    Add vouchers status livewire

---
+++ resources/views/livewire/voucher-status.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <div class="form-check form-switch">
+        <input class="form-check-input"  wire:model.lazy="isActive" type="checkbox" role="switch" @if($isActive) checked @endif>
+    </div>
+</div>
